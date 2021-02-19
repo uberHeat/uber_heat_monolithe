091b0ebaffd0fc4d3ece0dfe458c742c83189b80
Brice Michallski <brice_michalski@carrefour.com>
2021-02-19 11:57:28 +0100

    WIP

--- src/Dto/ConfigurationInput.php
+++ src/Dto/ConfigurationInput.php
@@ -3,8 +3,12 @@
 namespace App\Dto;
 
 use App\Entity\Dimension;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 final class ConfigurationInput
 {
+    /**
+     * @Groups({"configurationWrite"})
+     */
     public ?Dimension $dimension = null;
 }
